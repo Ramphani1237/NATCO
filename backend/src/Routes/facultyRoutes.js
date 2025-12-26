da4371c518eca24e7d5f5d60041dc85c6169499d
Ramphani1237 <ramphani05@gmail.com>
2025-12-26 16:52:52 +0530

    Fix ESLint unused variables

--- backend/src/Routes/facultyRoutes.js
+++ backend/src/Routes/facultyRoutes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { getProfile } = require("../Controllers/facultyController");
+const { getProfile } = require("../controllers/facultyController");
 const { protect } = require("../middleware/authMiddleware");
 
 router.get("/profile", protect, getProfile);
